7ac4f3c2d75ccd421bb54182f4ed104b0b0a8e79
Jimmy Selgen Nielsen <jimmy@Jimmys-MacBook-Pro.local>
2024-12-09 12:02:34 +0100

    Optimize Day 8 Solution

--- day8.py
+++ day8.py
@@ -8,22 +8,20 @@ def parse_input(filename):
                 ret[i+j*1j] = c
         return ret
     
-def part1(grid):
+
+def calculate(grid, niter=1):
     anti = []
     for freq in {*grid.values()} - {'.'}:
         pairs = permutations([p for p in grid if grid[p] == freq],2)
         anti += [x+i*(x-y) for x,y in pairs
-                           for i in [1]]
+                           for i in niter]
+        
     return len(set(anti) & set(grid))
+def part1(grid):
+    return calculate(grid,[1])
 
 def part2(grid):
-    anti = []
-    for freq in {*grid.values()} - {'.'}:
-        pairs = permutations([p for p in grid if grid[p] == freq],2)
-        anti += [x+i*(x-y) for x,y in pairs
-                           for i in range(50)]
-    return len(set(anti) & set(grid))
-
+    return calculate(grid,range(50))
 
 def main():
     grid = parse_input('input/day8.txt')
